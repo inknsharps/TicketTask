95de03df7ea73104238e207a4b776fa1d32b985b
inknsharps <nchen25@gmail.com>
2021-05-06 18:57:27 -0400

    add initial work on event routes api

---
+++ controllers/api/event-routes.js
@@ -0,0 +1,85 @@
+const router = require("express").Router();
+// const { Event } = require("../models");
+
+// Get all events, will need to adjust the render page once that is complete.
+router.get("/", async (req, res) => {
+   try {
+        const eventsData = await Event.findAll({
+            include: [
+                { 
+                    model: User,
+                    attributes: ["username"]
+                }
+            ]
+        });
+        const events = eventsData.map((event) => event.get({ plain: true }));
+        res.status(200).render("events", { events })
+   } catch (err) {
+        res.status(500).json(err);
+   } 
+});
+
+// Get event by event ID
+router.get("/:id", async (req, res) => {
+    try {
+        const eventData = await Event.findByPk(req.params.id, {
+            include: { 
+                model: User,
+                attributes: ["username"]
+            }
+        });
+        const event = eventData.get({ plain: true });
+        res.status(200).render("event", { event });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Make a new event
+// Make sure to include the information needed in the req.body
+router.post("/", async (req, res) => {
+    try {
+        const newEvent = await Event.create({
+            userId: req.body.userId,
+            eventName: req.body.eventName,
+            location: req.body.location,
+            eventDate: req.body.eventDate,
+            eventPrice: req.body.eventPrice
+        });
+        res.status(200).json(newEvent);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update an existing event, by its ID
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedEvent = await Event.update({
+            eventName: req.body.eventName,
+            location: req.body.location,
+            eventDate: req.body.eventDate,
+            eventPrice: req.body.eventPrice
+        },
+        {
+            where: { id: req.params.id }
+        });
+        res.status(200).json(updatedPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// Delete an existing event, by its ID
+router.delete("/:id", async (req, res) => {
+    try {
+        const deletedEvent = await Event.destroy({
+            where: { id: req.params.id }
+        });
+        res.status(200).json(deletedPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
