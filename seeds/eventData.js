a803019972467c6e6a55478448df9d80357d0512
inknsharps <nchen25@gmail.com>
2021-05-08 10:46:32 -0400

    update event routes to display relevant info

--- seeds/eventData.js
+++ seeds/eventData.js
@@ -3,16 +3,22 @@ const { Event } = require("../models");
 const eventData = [
     {
         eventName: "Neil's Event",
+        eventDate: "2021-06-15",
+        eventPrice: 25,
         userId: 1,
         locationId: 1,
     },
     {
         eventName: "Neil's Event Continued",
+        eventDate: "2021-06-16",
+        eventPrice: 25,
         userId: 1,
         locationId: 3
     },
     {
         eventName: "Gigi's Event",
+        eventDate: "2021-07-15",
+        eventPrice: 30,
         userId: 2,
         locationId: 2
     }
