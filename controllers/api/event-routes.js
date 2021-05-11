8ddb8a125bb223d22dff1cad7bd3b42c4ced00e9
Neil Chen <73395209+inknsharps@users.noreply.github.com>
2021-05-11 19:21:03 -0400

    Add search functionality routes for Location, and Event (#15)
    * add boilerplate code
    
    * update models to work with queries from the ticket model
    
    * initial work on email sending through nodemailer
    
    * reorganize emailer file structure, incorporate email functions into ticket routes
    
    * add nanoid to dependencies, change models to now include a ticket no, and update seeds data
    
    * update ticket emailing routes to now make use of ticketNo column
    
    * add dashboard routes
    
    * add search by name in event routes
    
    * add search route for event routes
    
    * add search via location route
    
    * get handlebars to render events and login
    
    * remove layout folder

--- controllers/api/event-routes.js
+++ controllers/api/event-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { Op } = require("sequelize");
 const { User, Event, Location } = require("../../models");
 
 // Get all events, will need to adjust the render page once that is complete.
@@ -30,7 +31,7 @@ router.get("/", async (req, res) => {
 });
 
 // Get event by event ID
-router.get("/:id", async (req, res) => {
+router.get("/id/:id", async (req, res) => {
     try {
         const eventData = await Event.findByPk(req.params.id, {
             include: [
@@ -54,6 +55,46 @@ router.get("/:id", async (req, res) => {
         res.status(200).json(event);
     } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+// Get/very fuzzy search for events (and its location) based on the event name
+// Query string pathing should be (without spaces):
+// /search? name=eventname &pricemax=50 &date=2022-02-22
+router.get("/search", async (req, res) => {
+    try {
+        const eventsSearched = await Event.findAll({
+            where: {
+                // We use OR to just look for everything at the moment in case a parameter is not passed
+                [Op.or]: [
+                    {
+                        eventName: {
+                            // Op.like should be good enough for this column
+                            [Op.like]: `%${req.query.name}%`
+                        }
+                    },
+                    {
+                        eventPrice: {
+                            // We use Op.lte as a less than whatever price is set to max
+                            [Op.lte]: [req.query.pricemax]
+                        },
+                    },
+                    {
+                        eventDate: {
+                            // Op.substring is used here since the date format in the database columns has extra stuff
+                            [Op.substring]: `%${req.query.date}%`
+                        }
+                    },
+                ]
+            },
+            include: {
+                model: Location,
+                as: "event_location"
+            }
+        });
+        res.status(200).json(eventsSearched);
+    } catch (err) {
+        res.status(400).json(err);
     }
 });
 
